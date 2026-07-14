ce4c7111db64ac9ded93ee0f33007762d2d13184
Kevin <92656833+kevinthegreat1@users.noreply.github.com>
2026-07-14 13:04:50 -0700

    Update accessor annotation target after locating target (#219)
    * Update accessor annotation target after locating target
    
    * Only update annotation
    
    * Update annotation to target field while keeping format
    
    * Apply changes from code review
    
    * Add target to invoker and remove desc from target string
    
    * Add descs back to `Accessor`/`Invoker` `target`s.
    
    ---------
    
    Co-authored-by: LlamaLad7 <l3gomindstorms@gmail.com>

--- src/main/java/org/spongepowered/asm/mixin/gen/AccessorInfo.java
+++ src/main/java/org/spongepowered/asm/mixin/gen/AccessorInfo.java
@@ -304,7 +304,6 @@ public class AccessorInfo extends SpecialMethodInfo {
         this.type = this.initType();
         this.targetFieldType = this.initTargetFieldType();
         this.target = this.initTarget();
-        this.annotation.visit("target", this.target.toString());
     }
 
     protected AccessorType initType() {
@@ -474,6 +473,7 @@ public class AccessorInfo extends SpecialMethodInfo {
      */
     public void locate() {
         this.targetField = this.findTargetField();
+        this.annotation.visit("target", this.targetField.name +  ':' + this.targetField.desc);
     }
 
     /**
--- src/main/java/org/spongepowered/asm/mixin/gen/InvokerInfo.java
+++ src/main/java/org/spongepowered/asm/mixin/gen/InvokerInfo.java
@@ -101,6 +101,7 @@ class InvokerInfo extends AccessorInfo {
     @Override
     public void locate() {
         this.targetMethod = this.findTargetMethod();
+        this.annotation.visit("target", this.targetMethod.name + this.targetMethod.desc);
     }
 
     private MethodNode findTargetMethod() {
