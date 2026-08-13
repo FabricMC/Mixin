9ed74a8344adf2e999ec18363dc79907d41dffc6
MildToucan <introvertpale@gmail.com>
2026-08-13 23:27:43 +0200

    Change to always throw for target errors if strict targeting is enabled
    Co-authored-by: Celeste <celdaemon@voidgroup.net>

--- src/main/java/org/spongepowered/asm/mixin/transformer/MixinInfo.java
+++ src/main/java/org/spongepowered/asm/mixin/transformer/MixinInfo.java
@@ -1054,8 +1054,7 @@ class MixinInfo implements Comparable<MixinInfo>, IMixinInfo {
     }
 
     private void handleTargetError(String message, boolean verboseOnly) {
-        boolean shouldThrow = FabricUtil.getCompatibility(this.parent) >= FabricUtil.COMPATIBILITY_0_17_4 ? this.parent.isRequired() : this.strict; 
-        if (shouldThrow) {
+        if (this.strict || (FabricUtil.getCompatibility(this.parent) >= FabricUtil.COMPATIBILITY_0_17_4 && this.parent.isRequired())) {
             this.logger.error(message);
             throw new InvalidMixinException(this, message);
         }
